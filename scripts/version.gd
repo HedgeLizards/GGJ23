798cf3f3542fa89df049ba7c89d0d14c04bcf626
troido <troido@protonmail.com>
2023-01-31 19:49:06 +0100

    show version in game

---
+++ scripts/version.gd
@@ -0,0 +1,12 @@
+extends Label
+
+
+const version_file = "res://version.txt"
+
+func _ready():
+	var file = File.new()
+	file.open(version_file, File.READ)
+	text = file.get_line()
+	file.close()
+	modulate.a = 0.5
+
