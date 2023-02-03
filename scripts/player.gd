bf892c2cc5b5dafa22f80e55db675eaa84b464aa
troido <troido@protonmail.com>
2023-02-03 21:07:18 +0100

    splitting

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,22 @@
 extends Node2D
 
 
-export var speed = 64
-export var rotation_speed = 1
+#export var speed = 64
+#export var rotation_speed = 1
+export var id = 0
+#var mirror = 1
 #var segments = PoolVector2Array()
 
 
-func _physics_process(delta):
-	var inp = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
-	$Germ.rotation += inp * delta * rotation_speed
-
-	var vel = Vector2(0, -speed).rotated($Germ.rotation)
-
-	$Germ.move_and_slide(vel)
-
-	if $Segments.get_point_count() == 0 || $Germ.position.distance_to($Segments.get_point_position($Segments.get_point_count() - 1)) > 5:
-		$Segments.add_point($Germ.position)
+#func __physics_process(delta):
+#	var inp = mirror * int(Input.is_action_pressed("right" + str(id))) - int(Input.is_action_pressed("left" + str(id)))
+#	$Germ/Tip.rotation += inp * delta * rotation_speed
+#
+#	var vel = Vector2(0, -speed).rotated($Germ/Tip.rotation)
+#
+#	$Germ/Tip.move_and_slide(vel)
+#
+#	if $Germ/Segments.get_point_count() == 0 || $Germ/Tip.position.distance_to($Germ/Segments.get_point_position($Germ/Segments.get_point_count() - 1)) > 5:
+#		$Germ/Segments.add_point($Germ/Tip.position)
 	
 	
