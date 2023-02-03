1edf779bae86d7ef8d2f098ee8183e36556bbcb8
troido <troido@protonmail.com>
2023-02-03 22:35:25 +0100

    branching after death

--- scripts/germ.gd
+++ scripts/germ.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-enum PlayerState {ALIVE, REVIVING}
+enum PlayerState {ALIVE, REVIVING, ABANDONED}
 
 export var speed = 64
 export var rotation_speed = 1
 var mirror = 1
 var state = PlayerState.ALIVE
+
 var target_id
 
 
@@ -13,9 +14,12 @@ var id
 func _ready():
 	id = get_parent().id
 
+func move_input():
+	return mirror * int(Input.is_action_pressed("right" + str(id))) - int(Input.is_action_pressed("left" + str(id)))
+
 func _physics_process(delta):
 	if state == PlayerState.ALIVE:
-		var inp = mirror * int(Input.is_action_pressed("right" + str(id))) - int(Input.is_action_pressed("left" + str(id)))
+		var inp = move_input()
 		$Tip.rotation += inp * delta * rotation_speed
 
 		var vel = Vector2(0, -speed).rotated($Tip.rotation)
@@ -33,13 +37,23 @@ func _physics_process(delta):
 			$Tip.position = target
 			target_id -= 1
 			target = $Segments.get_point_position(target_id)
+		$Tip.rotation = target.angle_to_point($Tip.position) - PI / 2
 		$Tip.position = $Tip.position.move_toward(target, d)
-		$Tip.rotation = target.angle_to($Tip.position)
 		
 		
 
 
 func _input(event):
+	if state == PlayerState.REVIVING and move_input() != 0:
+		var newGerm = self.duplicate()
+		newGerm.get_node("Tip").rotation += move_input() * 0.3
+		var new_points = $Segments.points
+		new_points.resize(target_id + 1)
+		newGerm.get_node("Segments").points = new_points
+		newGerm.state = PlayerState.ALIVE
+		state = PlayerState.ABANDONED
+		$Tip.visible = false
+		get_parent().add_child(newGerm)
 	if false and Input.is_action_just_pressed("power"+str(id)):
 		var newGerm = self.duplicate()
 		newGerm.mirror = -mirror
