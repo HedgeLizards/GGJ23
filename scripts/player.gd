3671483a48627957e31f09cdf25f10398cf221ff
troido <troido@protonmail.com>
2023-02-03 18:35:08 +0100

    basic player movement and line

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,19 @@
-extends KinematicBody2D
+extends Node2D
 
 
-export var speed = 256
+export var speed = 64
+export var rotation_speed = 1
+#var segments = PoolVector2Array()
 
-
-func _ready():
-	pass 
 
 func _physics_process(delta):
-	var inp = Vector2(
-		int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left")),
-		int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
-	)
-	var vel = inp.normalized() * speed
-	self.move_and_slide(vel)
+	var inp = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	$Germ.rotation += inp * delta * rotation_speed
+
+	var vel = Vector2(0, -speed).rotated($Germ.rotation)
+
+	$Germ.move_and_slide(vel)
+
+	if $Segments.get_point_count() == 0 || $Germ.position.distance_to($Segments.get_point_position($Segments.get_point_count() - 1)) > 5:
+		$Segments.add_point($Germ.position)
+		
